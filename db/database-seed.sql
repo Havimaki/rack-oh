2d6747f658ced454e184b90df2c6f01a40572926
Havimaki <dm.havimaki@gmail.com>
2022-06-18 13:35:59 -0400

    feature/install flyway

--- db/database-seed.sql
+++ db/database-seed.sql
@@ -1,7 +1,26 @@
-CREATE TABLE games
-(
-    id SERIAL,
-    sessionId integer
+
+-- create flyway schema table
+CREATE TABLE flyway_schema_history (
+    installed_rank integer PRIMARY KEY,
+    version character varying(50),
+    description character varying(200) NOT NULL,
+    type character varying(20) NOT NULL,
+    script character varying(1000) NOT NULL,
+    checksum integer,
+    installed_by character varying(100) NOT NULL,
+    installed_on timestamp without time zone NOT NULL DEFAULT now(),
+    execution_time integer NOT NULL,
+    success boolean NOT NULL
 );
 
-INSERT INTO games(sessionId) VALUES ('123');
+-- create indices for flyway schema table
+CREATE UNIQUE INDEX flyway_schema_history_pk ON flyway_schema_history(installed_rank int4_ops);
+CREATE INDEX flyway_schema_history_s_idx ON flyway_schema_history(success bool_ops);
+
+-- CREATE TABLE games
+-- (
+--     id SERIAL,
+--     sessionId integer
+-- );
+
+-- INSERT INTO games(sessionId) VALUES ('123');
